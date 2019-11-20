d23bb9911fd02f219157f285f90ae02f9181f145
taoshilai <taoshilai@x3platform.com>
2019-11-20 15:21:43 -0700

    update

--- tests/app_test_anjie.py
+++ tests/app_test_anjie.py
@@ -1,3 +1,4 @@
+#encoding=utf-8
 import requests
 import os,sys
 import numpy as np
@@ -29,3 +30,10 @@ if __name__=='__main__':
     t2=time.time()
     print('spend time:{}s'.format(t2-t1))
     print(res)
+'''
+spend time:4.950098514556885s
+{'message': 'success', 'code': '0', 'data': [{'answer': '硅片', 'question': 'GTIN'}, {'answer': '99.999999%', 'question': '加工程度'}, {'answer': '切片，标识，磨片，清洗，退火，粘片，抛光等，无GTIN，无CAS', 'question': '出口享惠情况'}, {'answer': '12英寸（约300MM）', 'question': '直径'}, {'answer': '单晶硅，掺杂硼元素，硅的纯度99.999999%。', 'question': '成分'}, {'answer': '圆形片状', 'question': '外观'}, {'answer': '硅片', 'question': '用途'}, {'answer': 'CAS', 'question': 'CAS'}, {'answer': '境外（其他）', 'question': '品牌类型'}]}
+
+spend time:13.27139139175415s
+{'data': [{'answer': '生产与日本，品牌是SUMCO牌，品牌类型是境外（其他）。', 'question': '用途'}, {'answer': '无CAS', 'question': 'CAS'}, {'answer': '12英寸', 'question': '直径'}, {'answer': '生产与日本，品牌是SUMCO牌，品牌类型是境外（其他）。', 'question': '出口享惠情况'}, {'answer': '圆形片状', 'question': '外观'}, {'answer': '加工工艺包括切片，标识，磨片，清洗，退火，粘片，抛光等', 'question': '加工程度'}, {'answer': '无GTIN，无CAS', 'question': 'GTIN'}, {'answer': '境外（其他）', 'question': '品牌类型'}, {'answer': '单晶硅', 'question': '成分'}], 'message': 'success', 'code': '0'}
+'''
